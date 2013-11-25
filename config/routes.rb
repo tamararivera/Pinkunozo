625328886f8e17973ba2fb6a45f274afc88b4975
Tamara Rivera <tamita.rivera@gmail.com>
2013-11-25 18:02:56 -0300

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Prototipo::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
-  match '/help',    to: 'static_pages#help', via: :get
-  match '/about',   to: 'static_pages#about', via: :get
+  match 'signup', to: 'users#new', via: :get
+  match '/help', to: 'static_pages#help', via: :get
+  match '/about', to: 'static_pages#about', via: :get
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
