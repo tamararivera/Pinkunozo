eb7cdb36138c8dc401dd59d1bc04f0bd9024b777
Tamara Rivera <tamita.rivera@gmail.com>
2013-12-05 10:32:52 -0300

    Change the default locale to spanish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       sign_in @user
     	flash[:success] = "Welcome to the Sample App!"
