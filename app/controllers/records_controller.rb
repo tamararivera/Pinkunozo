e15c7d7a55e34603a3a3abc75782a45f80b8f7f6
Tamara Rivera <tamita.rivera@gmail.com>
2013-12-17 00:49:10 -0300

    Change the style for the projects buttons (in the home page)

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,12 @@
 class RecordsController < ApplicationController
+  before_action :signed_in_user
   def index
     @project = Project.find(params[:id])
     @records_grid = initialize_grid(Record.where(project_id: params[:id]))
   end
+
+  private 
+    def project_params
+      params.require(:record).permit(:date, :secretary_id)
+    end
 end
