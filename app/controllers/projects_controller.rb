96137f92fe76825f06bf6376418132777d51111f
Tamara Rivera <tamita.rivera@gmail.com>
2014-04-15 02:52:32 -0300

    Add the create and edit project features

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,33 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      users = User.find(params[:project][:user_ids].reject(&:empty?))
+      @project.users << users
+
+      flash[:success] = 'Proyecto grabado con éxito'
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:success] = 'Proyecto grabado con éxito'
+      redirect_to root_path
+    end
+  end
   private 
     def project_params
       params.require(:project).permit(:name, :description)
